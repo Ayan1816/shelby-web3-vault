3968cba02980ecf9ba8476d6a0ec39279c1917c7
Ayan1816 <161149398+Ayan1816@users.noreply.github.com>
2026-06-09 04:14:34 +0600

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,30 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+        crypto: false,
+        stream: false,
+        path: false,
+        os: false,
+        zlib: false,
+        http: false,
+        https: false,
+        child_process: false
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
